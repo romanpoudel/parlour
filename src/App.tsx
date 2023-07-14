53f51b9d687d2196c0ade7addaae495db3f414c8
romanpoudel <romanpoudel325@gmail.com>
2023-07-14 12:22:24 +0545

    gallery skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Carousel from './components/Carousel'
+import Gallery from './components/Gallery'
 import Navbar from './components/Navbar'
 import Services from './components/Services'
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Carousel />
       <Services />
+      <Gallery />
 
     </div>
   )
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,55 @@
+
+
+const Gallery = () => {
+    var items = [
+        {
+            id: "1",
+            image: "images/services/pedicure.jfif",
+        },
+        {
+            id: "2",
+            image: "images/services/eyebrow.jfif",
+        },
+        {
+            id: "3",
+            image: "images/services/facial.jfif",
+        
+        },
+        {
+            id: "4",
+            image: "images/services/nailart.jfif",
+            
+        },
+        {
+            id: "5",
+            image: "images/services/hairstyles.jfif",
+            
+        },
+        {
+            id: "6",
+            image: "images/services/hairstyles.jfif",
+
+        }
+
+    ]
+    return (
+        <div className="flex flex-col items-center mt-6">
+            <div className="font-poppins text-2xl underline decoration-pink-500">
+                <text>Gallery</text>
+            </div>
+            <div className="grid grid-cols-3 gap-8 my-7">
+                {
+                    items.map((item) => (<img key={item.id} src={item.image} alt="" className=" w-[360px] h-[214px] hover:shadow-md hover:scale-105 object-cover " />))
+                }
+            </div>
+            <div>
+                <button
+                    className="text-white bg-pink-500 font-bold font-poppins text-xl rounded-full px-8 py-1">
+                    See More
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Gallery
